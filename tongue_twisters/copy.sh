ff8d849b1e8cffc41ef44657105a57db6c36553d
JooHo Lee <jlee@redhat.com>
2019-11-21 12:19:00 -0500

    2/3

--- tongue_twisters/copy.sh
+++ tongue_twisters/copy.sh
@@ -1,3 +1,7 @@
+# usage ./copy.sh new.md old.md
 cp template.md $1 
 
+title=$(echo $1|sed -r 's/_/ /g'| sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g'| awk -F. '{print  $2}')
+echo ${title}
+sed -i "s/%TITLE/$title/g" $1
 sed -i "s/%NEXT/$1/g" $2
